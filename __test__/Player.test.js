91dc72c376b0fcb7c479b377358a623765edc5e0
Habon Assoweh <habonassoweh@Habons-Air>
2022-03-12 01:26:45 -0500

    Player constructor completed

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -70,3 +70,34 @@ test("subtracts from player's health", () => {
 
   expect(player.health).toBe(0);
 });
+
+// test that verifies that a player's attack value is within range:
+test("gets player's attack value", () => {
+  const player = new Player("Dave");
+  player.strength = 10;
+
+  expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+//test that adds a potion to the inventory
+
+test("adds a potion to the inventory", () => {
+  const player = new Player("Dave");
+  const oldCount = player.inventory.length;
+
+  player.addPotion(new Potion());
+
+  expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+// test that uses a potion from inventory
+test("uses a potion from inventory", () => {
+  const player = new Player("Dave");
+  player.inventory = [new Potion(), new Potion(), new Potion()];
+  const oldCount = player.inventory.length;
+
+  player.usePotion(1);
+
+  expect(player.inventory.length).toBeLessThan(oldCount);
+});
